2f6509072dee70eeda5b938419b07a32017d799e
HSGamer <huynhqtienvtag@gmail.com>
2023-02-13 17:00:31 +0700

    auto fetch from official download server

--- legacy_convert.py
+++ legacy_convert.py
@@ -1,6 +1,9 @@
 import json
 import os
+import re
+import urllib.parse
 
+s = "https://ci.codemc.io/job/BetterGUI-MC/view/Addon/job/BungeeLink/101/artifact/target/BungeeLink-3.0.jar"
 
 def to_filename(s: str) -> str:
     new = ""
@@ -19,13 +22,19 @@ with open("addons.json", "r") as json_file:
 for name, values in addons.items():
     prop = {
         "name": name,
-        "version": values["version"],
         "description": values["description"],
         "author": values["authors"],
         "code": values["source-code"],
-        "download": values["direct-link"],
-        "wiki": values["wiki"]
+        "wiki": values["wiki"],
+        "type": "official"
     }
+    link = values["direct-link"]
+    link = urllib.parse.unquote(link, encoding='utf-8', errors='replace')
+    print(link)
+    matcher = re.search(r"https://ci.codemc.io/job/BetterGUI-MC(/view/Addon)?/job/([\s\w_-]+)/.+", link)
+    job = matcher.group(2)
+    print(job)
+    prop["jenkins"] = job
 
     with open(os.path.join("addons", to_filename(name) + ".json"), "w") as prop_file:
         prop_file.write(json.dumps(prop, indent=2))
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import json
 import os
+import re
+import urllib.request as request
+import urllib.parse as parse
+
+pattern = re.compile(r"(.+)-([\d.]+)\.jar")
 
 
 def read_properties(path: str) -> dict:
@@ -15,18 +20,52 @@ def read_folder(path: str) -> list[dict]:
     return properties_arr
 
 
+def fetch_from_official(jenkins_name: str) -> (str, str):
+    api_url = f"https://ci.codemc.io/job/BetterGUI-MC/job/{parse.quote(jenkins_name)}/api/json?tree=builds[url]"
+    print(f"Jenkins URL: {api_url}")
+    api_res = json.load(request.urlopen(api_url))
+    build_urls = [build["url"] for build in api_res["builds"]]
+    for build_url in build_urls:
+        normalized_build_url = build_url[0:-1] if build_url.endswith("/") else build_url
+        build_api_url = f"{normalized_build_url}/api/json?tree=artifacts[fileName,relativePath]"
+        print(f"Build URL: {build_api_url}")
+        build_res = json.load(request.urlopen(build_api_url))
+        artifacts = build_res["artifacts"]
+        for artifact in artifacts:
+            file_name = artifact["fileName"]
+            relative_path = artifact["relativePath"]
+            matcher = re.search(pattern, file_name)
+            try:
+                version = matcher.group(2)
+                artifact_url = f"{normalized_build_url}/artifact/{relative_path}"
+                print(f"Found: {file_name}")
+                return version, artifact_url
+            except:
+                continue
+    raise Exception("No download link & version found")
+
+
 def convert(properties: dict, file_extension: str = ".jar") -> (str, dict):
     prop_name = properties["name"]
     name = prop_name
+    print(f"Adding {name}")
     values = {
         "file-name": name + file_extension,
-        "version": properties["version"],
         "description": properties["description"],
         "authors": properties["author"],
         "source-code": properties["code"],
-        "direct-link": properties["download"],
         "wiki": properties["wiki"]
     }
+
+    prop_type = properties["type"] if "type" in properties else ""
+    if prop_type == "official":
+        jenkins_name = properties["jenkins"]
+        version, download_link = fetch_from_official(jenkins_name)
+        values["version"] = version
+        values["direct-link"] = download_link
+    else:
+        values["version"] = properties["version"]
+        values["direct-link"] = properties["download"]
 
     return name, values
 
